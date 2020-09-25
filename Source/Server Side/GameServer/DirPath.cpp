0fd0f399fdd2cce835bde7b34f2489c3c100f020
ptr0x <ptr0x@live.com>
2020-09-24 22:39:58 -0300

    Fixed minor problems in order to compile using G_V_S2_2 as game version.

--- Source/Server Side/GameServer/DirPath.cpp
+++ Source/Server Side/GameServer/DirPath.cpp
@@ -25,7 +25,7 @@ void CDirPath::SetFirstPath(char *dir_path)		// 절대경로 지정
 }
 
 
-char* CDirPath::GetNewPath(char *npath)			// 절대경로를 기준으로 상대경로 지정
+char* CDirPath::GetNewPath(const char *npath)			// 절대경로를 기준으로 상대경로 지정
 {
 	strcpy(m_newpath, m_path);
 	strcat(m_newpath, npath);
